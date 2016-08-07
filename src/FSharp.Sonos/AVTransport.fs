3e2002ca3189cd363b29fb66342913c226c58f62
Mathew Glodack <glodack@gmail.com>
2016-08-07 01:15:41 -0700

    Create AVTransport and Exception module

---
+++ src/FSharp.Sonos/AVTransport.fs
@@ -0,0 +1,24 @@
+﻿namespace FSharp.UPnP
+
+module AVTransport =
+  open FSharp.UPnP.Exceptions
+
+  let ServiceIdentifier = "urn:schemas-upnp-org:service:"
+  let ServiceType = "AVTransport:1"
+
+  type Action =
+  | Play
+  | Pause
+
+  let convertActionToString action =
+    match action with
+    | Play  -> "#Play"
+    | Pause -> "#Pause"
+
+  type InstanceID = InstanceID of int
+
+  let pause (instanceId : InstanceID) =
+    match instanceId with
+    | InstanceID id -> id
+    | _             -> raise (GenericException "TODO: PLEASE FIX ME :)")
+
---
+++ src/FSharp.Sonos/Exceptions.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.UPnP
+
+module Exceptions =
+  type Error =
+    {
+      ErrorCode : int;
+      ErrorDescription : string;
+      Description : string
+    }
+  exception GenericException of string
+  exception AVTransportException of Error
+
